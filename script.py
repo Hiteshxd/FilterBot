58218b2f1ca5ecf910a8be67340bba8887202430
ʜɪτєѕʜ <100434610+Hiteshxd@users.noreply.github.com>
2023-01-28 20:37:51 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 class Script(object):
 
-    START_MSG = """────「 [ᴢᴇʀᴏ ᴛᴡᴏ](https://te.legra.ph/file/581797978596af1e19a71.mp4) 」────
+    START_MSG = """────「 <a href="https://te.legra.ph/file/581797978596af1e19a71.mp4>ᴢᴇʀᴏ ᴛᴡᴏ</a> 」────
 やあ Kon'ichiwa {} 
 I'm Zero Two An Kawaii Advanced Unlimited Filter Bot.
 ───────────────────────
-  ♡ Try The More Button To Know More ♡
+Verified Under
+@KxOfficial || @MaximXTeam
 """
 
 
@@ -12,43 +13,39 @@ I'm Zero Two An Kawaii Advanced Unlimited Filter Bot.
 <i>Add Me As Admin In Your Group And Start Filtering</i>
 
 
-• <b>Basic Commands:</b>
-
-/start - Check if I'm alive!
-/help - Command help
-/about - Something about me!
-
-
-• <b>Filter Commands:</b>
-
-<code>/add name reply</code>  -  Add filter for name
-<code>/del name</code>  -  Delete filter
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-<code>/viewfilters</code>  -  List all filters in chat
+❏ <b> Basic Commands:</b>
+╠ /start - Check if I'm alive!
+╠ /help - Command help
+╚ /about - Something about me!
 
 
-• <b>Connection Commands:</b>
+❏ <b>Filter Commands:</b>
+╠ <code>/add name reply</code>  -  Add filter for name
+╠ <code>/del name</code>  -  Delete filter
+╠ <code>/delall</code>  -  Delete entire filters (Group Owner Only!)
+╚ <code>/viewfilters</code>  -  List all filters in chat
 
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-<code>/connections</code>  -  Manage your connections.
+
+❏ <b>Connection Commands:</b>
+╠ <code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
+╠ <code>/connect</code> in groups.
+╚ <code>/connections</code>  -  Manage your connections.
 
 
-• <b>Extras:</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-/id  -  Shows ID information
+❏ <b>Extras:</b>
+╠ /status  -  Shows current status of your bot (Auth User Only)
+╚ /id  -  Shows ID information
 
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
 
 <b>Verified Under @KxOfficial</b>
 """
 
 
     ABOUT_MSG = """
-× ️<b>My Name : Zero Two</b>
-× ️<b>Creater :</b> @KxOfficial 
+× ️<b>My Name : </b> Zero Two
+× ️<b>Creater :</b> @ikx7_a
+× ️<b>Network : </b> @KxOfficial 
 × ️<b>Language :</b> <code>Python3</code>
 × ️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 """
